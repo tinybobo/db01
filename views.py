ba9aeede249f5a2a192de3da1629ac89e00d6455
tinybobo <1031731953@qq.com>
2018-10-25 22:55:47 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from diango.shortcuts import redirect
+
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
+
